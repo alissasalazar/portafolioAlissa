80543a24cfcd64cccae4d4c78ce1c7f4770a904a
alissasalazar <salazargamarraa@gmail.com>
2023-08-12 17:10:24 -0500

    feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NameComponent } from './name/name.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NameComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
