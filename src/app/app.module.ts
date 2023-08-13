7152fc35c2edb759bdbdcc4f5185f6a827930583
frank rocha <frankalessandror@gmail.com>
2023-08-12 19:41:05 -0500

    feat2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NameComponent } from './name/name.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { TecnologiesComponent } from './tecnologies/tecnologies.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     NameComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    TecnologiesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tecnologies/tecnologies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologies',
+  templateUrl: './tecnologies.component.html',
+  styleUrls: ['./tecnologies.component.css']
+})
+export class TecnologiesComponent {
+
+}
